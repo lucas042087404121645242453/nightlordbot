56ed7fd9f0de6303c75588957ac8a14f4d76d351
lucas042087404121645242453 <32345860+lucas042087404121645242453@users.noreply.github.com>
2017-10-08 14:49:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client();
 
-bot.login('MzY2MjcxNjM1MDcxNjk2OTA3.DLrFEQ.1Ogw4xEpXdEKlxJmh7KQ-xwA1k0');
+bot.login('MzY2MzM3MDAyMzM5MDQxMjgx.DLu4jA.WYrFV_tSj7XmcNSctpFQYN6P-fg');
 
 bot.on('message', message => {
 let responseObject = {
-    "!testarbot" : "**BOT ONLINE - OLA MUNDO!**",
-    "!faustao" : "Ta pegando fogo bicho:fire:",
-    "!ryca" : "Eu não vou acabar presa sabe por que?Por que sou rica eu sou ricaaaaaaaaaaaaaaaaaaaaaaaaaa:joy:",
-    "!ajuda" : "!testarbot:Testa Para Ver Se o Bot Esta Online!                                                                                                                                                             !faustao:Imita sabe que sabe quem o nosso querido Faustão ta pegando fogo bichoooo :fire:                                                                                                                  !ryca:Sabe quem esse tambem esse bot imita?Sou Rycaaaaaaaaaaaaaaaaaaaaaaa!Isso mesmo ele tambem imita a famosa sou ryca:joy:"
+    "--testarbot" : "**BOT ONLINE - OLA MUNDO!**",
+    "--faustao" : "Ta pegando fogo bicho:fire:",
+    "--ryca" : "Eu não vou acabar presa sabe por que?Por que sou rica eu sou ricaaaaaaaaaaaaaaaaaaaaaaaaaa:joy:",
+    "--ajuda" : "!testarbot:Testa Para Ver Se o Bot Esta Online!                                                                                                                                                             !faustao:Imita sabe que sabe quem o nosso querido Faustão ta pegando fogo bichoooo :fire:                                                                                                                  !ryca:Sabe quem esse tambem esse bot imita?Sou Rycaaaaaaaaaaaaaaaaaaaaaaa!Isso mesmo ele tambem imita a famosa sou ryca:joy:"
 };
 
 if(responseObject[message.content]){
     message.channel.send(responseObject[message.content]);
+}
+
+if (message.content.startsWith("!roleta")){
+    randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
+    if(randomNumber == 5){
+        message.reply("Você Perdeu!Tente Novamente!");
+    }
+    else{
+        message.reply("Você ganhou!Parabens!:grinning:");
+    }
 }
 });
