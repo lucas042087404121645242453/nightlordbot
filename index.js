7bd7049f7617adf463dd8f4e6f3b1cc33615a72e
lucas042087404121645242453 <32345860+lucas042087404121645242453@users.noreply.github.com>
2017-10-11 09:21:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ bot.login('MzY2MjcxNjM1MDcxNjk2OTA3.DLv4Hw.GflRI146Q1cdEhbrXyiejrtPmGc');
 
 bot.on('message', message => {
 let responseObject = {
-    "--testarbot" : "**BOT ONLINE - OLA MUNDO!**",
-    "--faustao" : "Ta pegando fogo bicho:fire:",
-    "--ryca" : "Eu não vou acabar presa sabe por que?Por que sou rica eu sou ricaaaaaaaaaaaaaaaaaaaaaaaaaa:joy:",
-    "--ajuda" : "!testarbot:Testa Para Ver Se o Bot Esta Online!                                                                                                                                                             !faustao:Imita sabe que sabe quem o nosso querido Faustão ta pegando fogo bichoooo :fire:                                                                                                                  !ryca:Sabe quem esse tambem esse bot imita?Sou Rycaaaaaaaaaaaaaaaaaaaaaaa!Isso mesmo ele tambem imita a famosa sou ryca:joy:"
+    "!testarbot" : "**BOT ONLINE - OLA MUNDO!**",
+    "!faustao" : "Ta pegando fogo bicho:fire:",
+    "!ryca" : "Eu não vou acabar presa sabe por que?Por que sou rica eu sou ricaaaaaaaaaaaaaaaaaaaaaaaaaa:joy:",
+    "!ajuda" : "!testarbot:Testa Para Ver Se o Bot Esta Online!                                                                                                                                                             !faustao:Imita sabe que sabe quem o nosso querido Faustão ta pegando fogo bichoooo :fire:                                                                                                                  !ryca:Sabe quem esse tambem esse bot imita?Sou Rycaaaaaaaaaaaaaaaaaaaaaaa!Isso mesmo ele tambem imita a famosa sou ryca:joy:"
 };
 
 if(responseObject[message.content]){
